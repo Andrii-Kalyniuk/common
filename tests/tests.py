30a0d18a02ba575e0bb84f7916f8fa3d2a92a18d
AndriiKalyniuk <v13urnum@gmail.com>
2019-10-10 04:15:43 +0300

    Changed the method of testing errors for error massege cautching, subtests added

--- tests/tests.py
+++ tests/tests.py
@@ -6,9 +6,11 @@ from homework import Rectangle
 class RectangleTestCases(unittest.TestCase):
 
     def test_rectangle_init(self):
-        with self.assertRaises(ValueError) as bad_rectangle:
-            Rectangle(5, 0)
-        self.assertTrue("Can't create rectangle with such width and height" in str(bad_rectangle.exception))
+        bad_sides = [(5, 0), (-1, 2), (0, 0), (1, -5), (0, 5)]
+        for a, b in bad_sides:
+            with self.subTest(a=a, b=b):
+                with self.assertRaisesRegex(ValueError, "Can't create rectangle with such width and height"):
+                    Rectangle(a, b)
 
     def test_perimeter(self):
         rectangle = Rectangle(5, 10)
@@ -21,31 +23,34 @@ class RectangleTestCases(unittest.TestCase):
     def test_get_sum_of_corners(self):
         rectangle = Rectangle(10, 5)
         for i in range(5):
-            self.assertEqual(rectangle.get_sum_of_corners(i), i * 90)
+            with self.subTest(i=i):
+                self.assertEqual(rectangle.get_sum_of_corners(i), i * 90)
 
     def test_get_sum_of_corners_more_than_4(self):
         rectangle = Rectangle(10, 15)
-        with self.assertRaises(ValueError) as bad_corners:
+        with self.assertRaisesRegex(ValueError, 'Rectangle has only 4 corners'):
             rectangle.get_sum_of_corners(42)
-        self.assertTrue('Rectangle has only 4 corners' in str(bad_corners.exception))
 
     def test_get_radius_of_circumscribed_circle(self):
         side_a, side_b = 10.3, 15
         rectangle = Rectangle(side_a, side_b)
-        self.assertEqual(rectangle.get_radius_of_circumscribed_circle(), math.sqrt(side_a ** 2 + side_b ** 2) / 2)
+        expected = math.sqrt(side_a ** 2 + side_b ** 2) / 2
+        actual = rectangle.get_radius_of_circumscribed_circle()
+        self.assertEqual(actual, expected)
 
     def test_get_radius_of_inscribed_circle(self):
-        rectangle = Rectangle(10.0, 10.0)
-        expected = 10.0 / 2
+        side_a = side_b = 10.0
+        rectangle = Rectangle(side_a, side_b)
+        expected = side_a / 2.0
         actual = rectangle.get_radius_of_inscribed_circle()
         self.assertEqual(expected, actual)
 
     def test_get_radius_of_inscribed_circle_is_sides_equal(self):
         rectangle = Rectangle(10, 5)
-        with self.assertRaises(ValueError) as bad_sides:
+        with self.assertRaisesRegex(ValueError, "Can't inscribed circle in rectangle with such width and height"):
             rectangle.get_radius_of_inscribed_circle()
-        self.assertTrue("Can't inscribed circle in rectangle with such width and height" in str(bad_sides.exception))
 
 
 if __name__ == "__main__":
     unittest.main()
+
