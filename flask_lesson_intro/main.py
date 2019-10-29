220be6744ea199822a68e6a026c69b6e34483ac1
AndriiKalyniuk <v13urnum@gmail.com>
2019-10-29 04:13:28 +0200

    it's alive

--- flask_lesson_intro/main.py
+++ flask_lesson_intro/main.py
@@ -9,5 +9,16 @@ def get_home_page():
     return render_template("home.html", data=get_data())
 
 
+@app.route('/<item_from_img>')
+def get_item(item_from_img):
+    return render_template("item.html",
+                           item_from_img=item_from_img, data=get_data())
+
+
+@app.route('/author.html')
+def get_author_page():
+    return render_template("author.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
