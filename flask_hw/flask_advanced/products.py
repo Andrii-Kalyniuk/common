f0d2a0fdf8e76639004df793192beb7e0f3d0164
AndriiKalyniuk <v13urnum@gmail.com>
2019-11-05 21:36:29 +0200

    forms for filter and product addition

--- flask_hw/flask_advanced/products.py
+++ flask_hw/flask_advanced/products.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, Response, request
+from flask import Blueprint, render_template, request, redirect, url_for
 
 from database import DB
 from models import Product
@@ -11,24 +11,25 @@ products = Blueprint('products', __name__, url_prefix='/product',
 def get_all_products():
     products_all = [product.__dict__ for product in DB['products']]
     args = request.args.to_dict()
-    print(20*'.', args)
-    print(20*'.', request.form.to_dict())
     return render_template('all_products.html',
                            products_all=products_all, args=args)
 
 
+@products.route('/add', methods=['GET'])
+def show_add_product_form():
+    return render_template('add_product.html')
+
+
 @products.route('/', methods=['POST'])
 def add_product():
-    data = request.json
-    data_from_form = request.form.to_dict()
-    print(data_from_form)
+    data = request.form.to_dict()
     DB['products'].append(Product(name=data.get('name'),
                                   description=data.get('description'),
                                   img_name=data.get('img_name'),
                                   price=data.get('price')
                                   )
                           )
-    return render_template('add_product.html', data=data['name'])
+    return redirect(url_for('products.get_all_products'))
 
 
 @products.route('/<uuid:uu_id>')
