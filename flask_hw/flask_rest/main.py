adf98c888f34940ceae88c5f11ec631bd12ec4a7
AndriiKalyniuk <v13urnum@gmail.com>
2019-11-12 19:57:10 +0200

    rooms ready

--- flask_hw/flask_rest/main.py
+++ flask_hw/flask_rest/main.py
@@ -12,15 +12,18 @@ def db_setup():
     DB['rooms'] = []
     DB['tenants'] = []
     DB['staff'] = []
-    room42 = Room(42, 'vip', 'occupied', 1000.50)
-    DB['rooms'].append(room42)
+
+    DB['rooms'].append(Room(42, 'vip', 'closed', 1000.50))
+    DB['rooms'].append(Room(7, 'vip', 'available', 9000))
+    DB['rooms'].append(Room(69, 'vip', 'available', 150))
+
 
 
 db_setup()
 app = Flask(__name__)
 api = Api(app)
 api.add_resource(HealthCheck, '/_health_check')
-api.add_resource(Rooms, '/rooms', '/rooms/<room_id>')
+api.add_resource(Rooms, '/api/v0.1/rooms', '/api/v0.1/rooms/<id_>')
 
 if __name__ == "__main__":
     app.config.from_object(run_config())
--- flask_hw/flask_rest/routes/rooms.py
+++ flask_hw/flask_rest/routes/rooms.py
@@ -1,9 +1,15 @@
 from flask import request
-from flask_restful import Resource, fields
+from flask_restful import Resource, fields, reqparse
 
 from db.db import DB
 from models import Room
 
+parser = reqparse.RequestParser()
+parser.add_argument('number')
+parser.add_argument('level')
+parser.add_argument('status')
+parser.add_argument('price')
+# parser.add_argument('id')
 
 class Rooms(Resource):
     room_structure = {
@@ -14,15 +20,20 @@ class Rooms(Resource):
         "id": fields.String
     }
 
-    def get(self, room_id=None):
-        if room_id:
-            print(20*'*', room_id)
-            room = list(filter(lambda r: room_id == r.id, DB['rooms']))
+    def get(self, id_=None):
+        room_all = [room.__dict__ for room in DB['rooms']]
+        args = parser.parse_args()
+        print(20*'*', id_)
+        print(20*'*', args)
+        if id_:
+            room = list(filter(lambda r: id_ == r.id, DB['rooms']))
             if room:
                 return room[0].__dict__, 200
         else:
-            room_all = [room.__dict__ for room in DB['rooms']]
-            return room_all, 200
+            if args['status']:
+                return list(filter(lambda r: args['status'] == r['status'],
+                                   room_all)), args
+        return room_all, 200
 
     def post(self):
         data = request.json
@@ -30,4 +41,33 @@ class Rooms(Resource):
                                 data.get('level'),
                                 data.get('status'),
                                 data.get('price')))
-        return {"message": "room added"}, 200
+        return {"message": "room was added"}, 201
+
+    def patch(self, id_):
+        args = request.json
+        print(20*'*', id_)
+        print(20*'*', args)
+        if id_:
+            room_to_update = list(filter(lambda r: id_ == r.id, DB['rooms']))
+            if room_to_update
+                if args:
+                    upd_here = DB['rooms'].index(room_to_update[0])
+                    DB['rooms'][upd_here].number = args.get('number', DB['rooms'][upd_here].number)
+                    DB['rooms'][upd_here].level = args.get('level', DB['rooms'][upd_here].level)
+                    DB['rooms'][upd_here].status = args.get('status', DB['rooms'][upd_here].status)
+                    DB['rooms'][upd_here].price = args.get('price', DB['rooms'][upd_here].price)
+                    return {"message": "room was updated"}
+                else:
+
+            return {"message": "room was not found"}, 404
+
+    def put(self):
+        pass
+
+    def delete(self, id_=None):
+        if id_:
+            room_to_del = list(filter(lambda r: id_ == r.id, DB['rooms']))
+            if room_to_del:
+                DB['rooms'].remove(room_to_del[0])
+                return {"message": "room was deleted"}
+            return {"message": "room was not found"}, 404
