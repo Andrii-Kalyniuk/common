4b6e8c6f982c411ddbc3a4348339357477803005
AndriiKalyniuk <v13urnum@gmail.com>
2019-11-19 23:57:10 +0200

    Lets switch to ORM. Config edited for postgres DB

--- flask_hw/flask_rest/config.py
+++ flask_hw/flask_rest/config.py
@@ -3,10 +3,18 @@ import os
 
 class Config:
     DEBUG = False
+    PG_USER = 'cursor'
+    PG_PASSWORD = 'very_secret_password'
+    PG_HOST = 'localhost'
+    PG_PORT = '5432'
+    DB_NAME = 'test_orm'
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{DB_NAME}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class TestConfig(Config):
     DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 class ProdConfig(Config):
