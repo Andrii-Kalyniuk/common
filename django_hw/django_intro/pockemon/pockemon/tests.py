011691989eb70a10c249f1a96d379fe3eca6f0e3
andrii-kalyniuk <v13urnum@gmail.com>
2020-01-10 23:07:06 +0200

    test.py mistype removed

--- django_hw/django_intro/pockemon/pockemon/tests.py
+++ django_hw/django_intro/pockemon/pockemon/tests.py
@@ -14,6 +14,6 @@ class StatusViewTests(TestCase):
         assert response.status_code == HTTPStatus.OK
 
     def test_index_page(self):
-        response = self.client.g\eet(reverse('index'), content_type='application/json')
+        response = self.client.get(reverse('index'), content_type='application/json')
         print(response.templates)
         self.assertEqual(response.status_code, 200)
